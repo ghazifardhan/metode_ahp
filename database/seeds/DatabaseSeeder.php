4b33b47d3feeb73ba157fb5f6131d8220f1b2ca6
Ghazi Fadil <ghazideveloper@gmail.com>
2017-06-13 15:36:08 +0700

    fix bugs

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,6 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
         $this->call(table_importance_level::class);
-        $this->call(TableRCISeeder::class);
+        //$this->call(TableRCISeeder::class);
     }
 }
